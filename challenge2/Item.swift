a57c20f8684c14b9706f9a14158b5eff0c9d189a
jiwonpark831 <jiwonpark831@gmail.com>
2025-04-14 10:22:02 +0900

    Initial Commit

---
+++ challenge2/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  challenge2
+//
+//  Created by jiwon on 4/14/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ challenge2/challenge2App.swift
@@ -0,0 +1,32 @@
+//
+//  challenge2App.swift
+//  challenge2
+//
+//  Created by jiwon on 4/14/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct challenge2App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
